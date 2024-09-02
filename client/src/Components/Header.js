fb7448389d2af0ac9811aa00e5273e13a15e0936
Ogbonna Blessed <blessedlyrics11@gmail.com>
2024-09-02 07:35:53 +0100

    update header component with user data

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink, Link } from 'react-router-dom'
-import { FaMoon, FaSearch } from 'react-icons/fa'
+import { FaMoon, FaSearch, FaUser } from 'react-icons/fa'
+import { useSelector } from 'react-redux'
 
 const Header = () => {
+    const { currentUser } = useSelector(state => state.user);
+    const [userInfo, setUserInfo] = useState(false);
+
+    const displayInfo = () => {
+        setUserInfo(!userInfo);
+    }
+
   return (
     <div className='nav-container'>
         <div className="nav-box">
@@ -23,9 +31,35 @@ const Header = () => {
             <div className="background-toggle-box">
                 <FaMoon size={18}/>
             </div>
-            <Link to='/signup'>
-                <button type="button">Sign up</button>
-            </Link>
+            {
+                currentUser 
+                ? (
+                    <div className="user-info-container">
+                        <div className='user-info' onClick={displayInfo}>
+                            {
+                                currentUser.profilePicture 
+                                ? <img src={currentUser.profilePicture} alt="" />
+                                : <FaUser className='user-icon' size={25} />
+                            }
+                        </div>
+                        {userInfo &&
+                        <div className="user-info-drop-down">
+                            <div className="user-name">{currentUser.username}</div>
+                            <div className="email">{currentUser.email}</div>
+                            <Link to="/Dashboard?=profile">Profile</Link>
+                            <button type='button'>Log out</button>
+                        </div>
+                        }
+                    </div>
+                ) 
+                : (
+                    <>
+                    <Link to='/signup'>
+                        <button type="button">Sign up</button>
+                    </Link>
+                    </>
+                )
+            }
         </div>
     </div>
   )
