916feee6c101690d515144359989653ae41db745
Ogbonna Blessed <blessedlyrics11@gmail.com>
2024-09-05 05:25:27 +0100

    add create post route

---
+++ api/models/post.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        require: true,
+    },
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    image: {
+        type: String,
+        default: 'https://verpex.com/assets/uploads/images/blog/How-Many-Blogs-Should-You-Post-a-Wee.webp?v=1678462713',
+    },
+    category: {
+        type: String,
+        default: 'uncategorized',
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+}, { timestamps: true });
+
+const Post = mongoose.model('Post', postSchema);
+
+export default Post;
